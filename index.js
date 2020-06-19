394faa43c061ee4e1e215a277b62ebb78a23165e
BlackMonkey56 <blackmonkey9256@gmail.com>
2020-06-20 00:20:14 +0900

    Set middlewares(morgan, helmet, cooke-parser, body-parser)

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = 3000;
@@ -6,6 +9,12 @@ const port = 3000;
 const handleHome = (req, res) => res.send('Hello from Home');
 
 const handleProfile = (req, res) => res.send('You are on my profile');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(morgan('dev'));
 
 app.get('/', handleHome);
 
