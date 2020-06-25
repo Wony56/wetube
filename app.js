b2158d27294714f69b9e72a27354f0450740e701
BlackMonkey56 <blackmonkey9256@gmail.com>
2020-06-26 00:49:37 +0900

    Add globalRouter, userRouter, videoRouter & routes

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
-import { userRouter } from './router';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
+import globalRouter from './routers/globalRouter';
+import routes from './routes';
 
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello from Home');
-
-const handleProfile = (req, res) => res.send('You are on my profile');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,17 +15,8 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(morgan('dev'));
 
-const middleware = (req, res, next) => {
-  console.log('Helloe middleware');
-  next();
-};
-
-app.use(middleware);
-
-app.get('/', handleHome);
-
-app.get('/profile', handleProfile);
-
-app.use('/user', userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
